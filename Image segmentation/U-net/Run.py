7c3e5257ef8ee31ffb4be489bfc3fff5ec2c3ee6
sphyse <keyburn1995@gmail.com>
2021-01-25 10:08:47 +0000

    No transform

--- Image segmentation/U-net/Run.py
+++ Image segmentation/U-net/Run.py
@@ -1,11 +1,9 @@
-import matplotlib.pyplot as plt
-import numpy as np
 from torch.utils.data import DataLoader
 from UnetDataLoader import Unetdataload, PicDataset
 from model import SimpleUnet
 import torch.nn as nn
 import torch.optim as optim
-from torchvision.transforms import Compose, ToTensor, Normalize
+
 
 train_image_dir = "data/membrane/train/image/"
 train_label_dir = "data/membrane/train/label/"
@@ -15,8 +13,9 @@ plt = unetdata.preview()
 
 
 Unet_dataset = unetdata.getdataset()
-Unet_dataloader = DataLoader(Unet_dataset, batch_size=16, shuffle=True,)
+Unet_dataloader = DataLoader(Unet_dataset, batch_size=4, shuffle=True)
 Unet = SimpleUnet()
+Unet = Unet.cuda()
 lr = 0.003
 optimizer = optim.Adam(Unet.parameters(), lr=lr)
 criterion = nn.MSELoss()
@@ -25,12 +24,12 @@ for e in range(epochs):
     sum_loss = 0.0
     for _, item in enumerate(Unet_dataloader):
         data, label = item
-        output = Unet(data)
-        loss = criterion(output, label)
+        output = Unet(data.cuda())
+        loss = criterion(output, label.cuda())
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         sum_loss += loss
-        if _ % 100 == 99:
-            print(sum_loss / 100)
+        if _ % 5 == 4:
+            print(sum_loss / 5)
             sum_loss = 0.0
--- Image segmentation/U-net/UnetDataLoader.py
+++ Image segmentation/U-net/UnetDataLoader.py
@@ -1,10 +1,9 @@
 import torch as tr
 import os
-from matplotlib import image
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader, Dataset
-from torchvision.transforms import functional as tvF
-
+from torchvision.transforms import CenterCrop,ToTensor
+from PIL import Image
 
 class Unetdataload():
     def __init__(self, train_image_dir, train_label_dir):
@@ -19,9 +18,9 @@ class Unetdataload():
         for i in range(preview_number):
             image_file_path = os.path.join(self.train_image_folder_path, self.train_image_fileName_list[i])
             label_file_path = os.path.join(self.train_label_folder_path, self.train_image_fileName_list[i])
-            preview_image_data = image.imread(image_file_path)
+            preview_image_data = Image.open(image_file_path)
             preview_images_list.append(preview_image_data)
-            preview_label_data = image.imread(label_file_path)
+            preview_label_data = Image.open(label_file_path)
             preview_label_list.append(preview_label_data)
         for i in range(preview_number):
             plt.subplot(2, preview_number, i + 1)
@@ -48,10 +47,11 @@ class PicDataset(Dataset):
     def __getitem__(self, idx):
         data_file_path = os.path.join(self.data_path, self.file_name_list[idx])
         label_file_path = os.path.join(self.data_path, self.file_name_list[idx])
-        data = image.imread(data_file_path)
-        label = image.imread(label_file_path)
-        data = tvF.to_tensor(data)
-        label = tvF.to_tensor(label)
+        data = Image.open(data_file_path)
+        label = Image.open(label_file_path)
+        totensor = ToTensor()
+        data = totensor(data)
+        label = totensor(label)
         if self.transform is not None:
             data = tr.tensor(data)
             label = tr.tensor(label)
@@ -62,6 +62,6 @@ class PicDataset(Dataset):
             label = tr.tensor(label)
 
         data = data.view(1,512, 512)
-        label = tvF.center_crop(label, 322)
+        label = label[:,95:417,95:417]
 
-        return data, label
+        return (data, label)
