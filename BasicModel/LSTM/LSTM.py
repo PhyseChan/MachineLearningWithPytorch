ecbabc4189b1558bbe7d1fb8e27459fb579cc263
PhyseChan <physechan@gmail.com>
2021-02-27 12:24:39 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Image segmentation/U-net/Run.py

--- BasicModel/LSTM/LSTM.py
+++ BasicModel/LSTM/LSTM.py
@@ -1,6 +1,5 @@
 import torch as tr
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class LSTM_cell(nn.Module):
@@ -19,12 +18,12 @@ class LSTM_cell(nn.Module):
         self.woin = nn.Linear(input_size, output_size)
 
     def forward(self, x, h, c):
-        f = F.sigmoid(self.wfh(h) + self.wfin(x))
-        i = F.sigmoid(self.wih(h) + self.wiin(x))
-        c_ = F.tanh(self.wch(h) + self.wcin(x))
-        o = F.sigmoid(self.woh(h) + self.woin(x))
+        f = tr.sigmoid(self.wfh(h) + self.wfin(x))
+        i = tr.sigmoid(self.wih(h) + self.wiin(x))
+        c_ = tr.tanh(self.wch(h) + self.wcin(x))
+        o = tr.sigmoid(self.woh(h) + self.woin(x))
         c_next = f * c + i * c_
-        h_next = F.tanh(c_next) * o
+        h_next = tr.tanh(c_next) * o
 
         return h_next, c_next
 
@@ -46,17 +45,6 @@ class LSTM(nn.Module):
             h, c = self.lstm_cell_1(x[:, i, :].unsqueeze(1), h, c)
         h = self.linear(h)
         return h
-#     def __init__(self, in_size,hidden_size, out_size):
-#         super(LSTM, self).__init__()
-#         self.lstm_cell_1 = nn.LSTM(in_size, hidden_size,2)
-#         self.fc = nn.Linear(hidden_size, out_size )
-#
-#     def forward(self, x, h0, c0):
-# #        sequence_len = x.shape[1]
-#         c = c0
-#         y = h0
-#         x = x.permute(1,0,2)
-#         y, c = self.lstm_cell_1(x)
-#         y = self.fc(y[-1,:,:])
-#         return y
+
+
 
--- BasicModel/LSTM/Run.py
+++ BasicModel/LSTM/Run.py
@@ -9,7 +9,7 @@ import numpy as np
 training_loader, test_loader = MnistReader().dataloader(batch_size_trian= 50)
 
 epochs = 1
-model = LSTM(28, 128,10)
+model = LSTM(28, 32,10)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 for e in range(epochs):
@@ -38,5 +38,13 @@ for _, item in enumerate(test_loader):
     test_label_list += label_test.tolist()
     test_predict_list += pred.tolist()
 
+accuracy = metrics.accuracy_score(test_predict_list,test_label_list)
+precision = metrics.precision_score(test_predict_list,test_label_list,average='macro')
+recall = metrics.recall_score(test_predict_list,test_label_list,average='macro')
+f1 = metrics.f1_score(test_predict_list,test_label_list,average='macro' )
+print("accuracy: {:f}".format(accuracy))
+print("precision: {:f}".format(precision))
+print("recall: {:f}".format(recall))
+print("f1 score: {:f}".format(f1))
 
 
